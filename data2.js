1a3477b8fbaedf9247bd644a776d0675c3f20a2b
FakeandrewA <143100343+FakeandrewA@users.noreply.github.com>
2023-11-25 21:14:58 +0530

    Update data2.js

--- data2.js
+++ data2.js
@@ -3059,3 +3059,28 @@ function fetchTheData()
 }
 
 //#############################//################//##################//#####################//########################//######################//########################
+
+//A function to restrict past days from being selected
+
+function pastDays()
+  {
+    var dtToday = new Date();
+    
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+    
+    var maxDate = year + '-' + month + '-' + day;
+
+    // or instead:
+    // var maxDate = dtToday.toISOString().substr(0, 10);
+
+    $('#txtDate').attr('min', maxDate);
+    
+}
+
+//#############################//################////#############################//################//##################//#####################//########################//######################//########################
